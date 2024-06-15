fc84eb6df89251a767af197671246f2d09ca2e62
Jorge Asus <jorgehn1995@gmail.com>
2024-06-15 02:39:43 -0600

    se reparo el ingreso de usuarios

--- src/main/java/com/umg/examen/utils/Runner.java
+++ src/main/java/com/umg/examen/utils/Runner.java
@@ -35,14 +35,23 @@ public class Runner implements CommandLineRunner {
                 new Authority(AuthorityName.ROLE_USER),
                 new Authority(AuthorityName.ROLE_ADMIN)
         ));
-        User user = new User("admin",
-                new BCryptPasswordEncoder().encode("password"),
-                List.of(
-                        this.authorityRepository.findByName(
-                                AuthorityName.ADMIN
+        this.userRepository.saveAll(List.of(
+                new User("el_inge",
+                        new BCryptPasswordEncoder().encode("pass123"),
+                        List.of(
+                                this.authorityRepository.findByName(
+                                        AuthorityName.ROLE_ADMIN
+                                )
+                        )
+                ),
+                new User("el_estudiante",
+                        new BCryptPasswordEncoder().encode("pass123"),
+                        List.of(
+                                this.authorityRepository.findByName(
+                                        AuthorityName.ROLE_USER
+                                )
                         )
                 )
-        );
-        this.userRepository.save(user);
+        ));
     }
 }
